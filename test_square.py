586c9bf965249786e53d12f6aa5fea6c3b80a9c0
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-08 00:05:13 +0700

    Testing expected exceptions

--- test_square.py
+++ test_square.py
@@ -4,6 +4,14 @@ from unittest import main
 from square import Square
 
 class TestSquare(TestCase):
+  def test_init_raises_type_error(self):
+    with self.assertRaises(TypeError):
+      square = Square("10")
+
+  def test_init_raises_value_error(self):
+    with self.assertRaises(ValueError):
+      square = Square(-10)
+
   def test_area(self):
     square = Square(10)
     area = square.area()
